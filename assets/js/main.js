48af55e092f7230d2f81f1688daebe8731533aa7
Yair G <yjgarizabalo@gmail.com>
2022-07-15 11:35:58 -0500

    [YaGr] update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,11 @@ let menu = document.querySelector('.menu-icon');
 menu.onclick = () => {
     menu.classList.toggle("move")
 }
+
+// SWIPER - MENU
+
+// var swiper = new Swiper(".reviews-content", {
+//     pagination: {
+//         el: ".swiper-pagination",
+//     },
+// });
